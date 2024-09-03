342fd8dbdf4847ec43b068c5e01850a8cd95febb
santiagoRamirez3 <santiago.ramirez3@udea.edu.co>
2024-09-03 14:02:17 -0500

    Sample of 5 days cooling try

--- Arduino_executable.py
+++ Arduino_executable.py
@@ -2,8 +2,8 @@ from Arduino_class import *
 
 def temperature_function(hours):
     # Ejemplo: decrecimiento del setpoint cada 2 horas
-    base_temperature = 55.  # Temperatura base en grados Celsius
-    increment = -0.25  # decrecimiento de temperatura cada hora
+    base_temperature = 49.  # Temperatura base en grados Celsius
+    increment = -0.2  # decrecimiento de temperatura cada hora
     return round(base_temperature + (increment * (hours // 1)),2)
 
 control_system = Control(cooling_function = temperature_function,
